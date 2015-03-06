7029dc55182284b9d492bc94efa3425facaac767
Adam Hellberg <sharparam@sharparam.com>
2015-03-07 00:03:35 +0100

    Add AssemblyInfo file.

---
+++ Sharparam.Scroller/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sharparam.Scroller")]
+[assembly: AssemblyDescription("Game made with SFML")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sharparam")]
+[assembly: AssemblyProduct("Sharparam.Scroller")]
+[assembly: AssemblyCopyright("Copyright (c) 2015 by Adam Hellberg.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94db932a-09da-4a11-9955-4f9971cf8f0f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
