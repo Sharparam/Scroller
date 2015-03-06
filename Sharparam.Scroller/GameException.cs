29f7b29262cc0f1ead6da58dddf77f257198f242
Adam Hellberg <sharparam@sharparam.com>
2015-03-07 00:02:11 +0100

    Add custom exception classes.

---
+++ Sharparam.Scroller/GameException.cs
@@ -0,0 +1,23 @@
+﻿namespace Sharparam.Scroller
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    using log4net;
+
+    public class GameException : Exception
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(GameException));
+
+        public GameException(string message = null, Exception innerException = null)
+            : base(message, innerException)
+        {
+            Log.Error("New GameException created.", this);
+        }
+
+        protected GameException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ Sharparam.Scroller/MapLoadException.cs
@@ -0,0 +1,20 @@
+﻿namespace Sharparam.Scroller
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    using log4net;
+
+    public class MapLoadException : GameException
+    {
+        public MapLoadException(string message = null, Exception innerException = null)
+            : base(message, innerException)
+        {
+        }
+
+        protected MapLoadException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
