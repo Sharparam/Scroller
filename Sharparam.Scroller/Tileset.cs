293e02d1a05da31d6ad9ebaa6141fe283b708df5
Adam Hellberg <sharparam@sharparam.com>
2015-03-07 13:37:42 +0100

    Improve tileset loading and features.

--- Sharparam.Scroller/Tileset.cs
+++ Sharparam.Scroller/Tileset.cs
@@ -1,7 +1,8 @@
 ﻿namespace Sharparam.Scroller
 {
+    using System;
     using System.Collections.Generic;
-    using System.Reflection.Emit;
+    using System.Diagnostics;
 
     using log4net;
 
@@ -14,41 +15,108 @@
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof(Tileset));
 
-        private readonly TmxTileset _tmxTileset;
+        private readonly Dictionary<int, Vector2f> _coordCache;
+        
+        private readonly int _firstGid;
+        
+        private readonly int _height;
 
-        private readonly string _imageFile;
+        private readonly int _lastGid;
+
+        private readonly string _name;
+
+        private readonly bool _preloaded;
 
         private readonly Texture _texture;
 
-        private readonly int _firstGid;
+        private readonly int _tileCount;
+        
+        private readonly int _tilesX;
+        
+        private readonly int _tilesY;
+        
+        private readonly TmxTileset _tmxTileset;
 
-        private readonly uint _width;
+        private readonly int _width;
 
-        private readonly uint _height;
-
-        private readonly uint _tilesX;
-
-        private readonly uint _tilesY;
-
-        private readonly Dictionary<int, Vector2f> _coordCache; 
-
-        public Tileset(TmxTileset tmxTileset)
+        public Tileset(TmxTileset tmxTileset, bool preload = true)
         {
             _tmxTileset = tmxTileset;
-            Log.InfoFormat("Loading tileset {0}, image: {1}", _tmxTileset.Name, _tmxTileset.Image.Source);
-            Log.Debug("Loading tileset texture from TMX tileset image stream.");
-            var image = new Image(_tmxTileset.Image.Source);
+            _name = _tmxTileset.Name;
+            var tmxImage = _tmxTileset.Image;
+            Log.InfoFormat("Loading tileset {0}, image: {1}", _name, tmxImage.Source ?? "data stream");
+
+            _firstGid = _tmxTileset.FirstGid;
+            Log.DebugFormat("First GID is {0}", _firstGid);
+
+            Log.Debug("Loading tileset texture from TMX tileset image...");
+
+            if (!tmxImage.Height.HasValue)
+                throw new TilesetException("Tileset image has no height!");
+            if (!tmxImage.Width.HasValue)
+                throw new TilesetException("Tileset image has no width!");
+            _height = tmxImage.Height.Value;
+            _width = tmxImage.Width.Value;
+            Log.DebugFormat("Tileset image is {0}x{1}", _width, _height);
+
+            Image image;
+            if (tmxImage.Source == null)
+            {
+                Log.Debug("Loading image from embedded data string.");
+                image = new Image(tmxImage.Data);
+            }
+            else
+            {
+                Log.Debug("Loading image from file.");
+                image = new Image(_tmxTileset.Image.Source);
+            }
+
+            Debug.Assert(image.Size.X == _width, "Loaded image width is different from source image width.");
+            Debug.Assert(image.Size.Y == _height, "Loaded image height is different from source image height.");
+
             var trans = _tmxTileset.Image.Trans;
             if (trans != null)
                 image.CreateMaskFromColor(new Color((byte)trans.R, (byte)trans.G, (byte)trans.B));
+
             _texture = new Texture(image);
-            _firstGid = _tmxTileset.FirstGid;
-            _width = _texture.Size.X;
-            _height = _texture.Size.Y;
+
+            Debug.Assert(_texture.Size.X == _width, "Loaded texture width is different from source image width.");
+            Debug.Assert(_texture.Size.Y == _height, "Loaded texture height is different from source image height.");
+
             Log.DebugFormat("Tileset texture loaded, dimensions: {0},{1}", _width, _height);
-            _tilesX = _width / (uint)_tmxTileset.TileWidth;
-            _tilesY = _height / (uint)_tmxTileset.TileHeight;
-            _coordCache = new Dictionary<int, Vector2f>(_tmxTileset.Tiles.Count);
+            _tilesX = _width / _tmxTileset.TileWidth;
+            _tilesY = _height / _tmxTileset.TileHeight;
+            _tileCount = _tmxTileset.Tiles.Count;
+            Debug.Assert(_tileCount == _tilesX * _tilesY, "Tile count given by TMX is different from calculated count.");
+            Log.DebugFormat("Tileset has {0}x{1} ({2}) tiles.", _tilesX, _tilesY, _tileCount);
+            _lastGid = _firstGid + _tileCount - 1;
+            Log.Debug("Creating coord cache.");
+            _coordCache = new Dictionary<int, Vector2f>(_tileCount);
+            if (!preload)
+            {
+                _preloaded = false;
+                return;
+            }
+            Log.Debug("Populating coord cache.");
+            for (var id = _firstGid; id <= _lastGid; id++)
+                GetTileCoordinates(id);
+            _preloaded = true;
+        }
+
+        public int FirstGid
+        {
+            get
+            {
+                return _firstGid;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
         }
 
         public Texture Texture
@@ -59,10 +127,65 @@
             }
         }
 
+        public int TileCount
+        {
+            get
+            {
+                return _tileCount;
+            }
+        }
+
+        public int TileHeight
+        {
+            get
+            {
+                return _tmxTileset.TileHeight;
+            }
+        }
+
+        public int TileWidth
+        {
+            get
+            {
+                return _tmxTileset.TileWidth;
+            }
+        }
+
+        public int TilesX
+        {
+            get
+            {
+                return _tilesX;
+            }
+        }
+
+        public int TilesY
+        {
+            get
+            {
+                return _tilesY;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+        }
+
         public Vector2f GetTileCoordinates(int gid)
         {
+            if (gid < _firstGid || gid > _lastGid)
+                throw new ArgumentOutOfRangeException("gid", gid, "GID is outside the range of defined tileset GIDs.");
             if (_coordCache.ContainsKey(gid))
                 return _coordCache[gid];
+            if (_preloaded)
+                Log.WarnFormat(
+                    "Tile coordinates are preloaded but GetTileCoordinates for {0} is calculating tile coordinates for GID {1}",
+                    _name,
+                    gid);
             var sheetIndex = gid - _firstGid;
             var row = sheetIndex / _tilesX;
             var col = sheetIndex % _tilesX;
---
+++ Sharparam.Scroller/TilesetException.cs
@@ -0,0 +1,18 @@
+﻿namespace Sharparam.Scroller
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    public class TilesetException : GameException
+    {
+        public TilesetException(string message = null, Exception innerException = null)
+            : base(message, innerException)
+        {
+        }
+
+        protected TilesetException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
